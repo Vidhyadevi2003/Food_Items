593026eed9ff86ffda10d9ea2a0a666cb89a7b77
Vidhyadevi2003 <78740193+Vidhyadevi2003@users.noreply.github.com>
2021-04-02 16:45:44 +0530

    Created Food_items.c

---
+++ Food_items.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+	int choice;
+	char F_item[20],price[5];
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1: strcpy(F_item,"Pizza");
+		strcpy(price,"Rs 239");
+		printf("Food item- ");
+		puts(F_item);
+		printf("\n Price- ");
+		puts(price);
+		break;
+		case 2: strcpy(F_item,"Burger");
+		strcpy(price,"Rs 129");
+		printf("Food item- ");
+		puts(F_item);
+		printf("\n Price- ");
+		puts(price);
+		break;
+		case 3: strcpy(F_item,"Pasta");
+		strcpy(price,"Rs 179");
+		printf("Food item- ");
+		puts(F_item);
+		printf("\n Price- ");
+		puts(price);
+		break;
+		case 4: strcpy(F_item,"French fries");
+		strcpy(price,"Rs 99");
+		printf("Food item- ");
+		puts(F_item);
+		printf("\n Price- ");
+		puts(price);
+		break;
+		case 5: strcpy(F_item,"Sandwich");
+		strcpy(price,"Rs 149");
+		printf("Food item- ");
+		puts(F_item);
+		printf("\n Price- ");
+		puts(price);
+		break;
+	}
+	return 0;
+}
+			
+	
